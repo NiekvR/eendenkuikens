927e53e9f8feb9c5119c48f685c7eae592e59a72
NiekvR <Niek@Nieks-MacBook-Pro.local>
2016-05-03 20:51:26 +0200

    Add extra console line

--- config/mongoose.js
+++ config/mongoose.js
@@ -10,6 +10,11 @@ module.exports = function() {
     require('../app/models/sighting.server.model');
     console.log("Con 1 "+ mongoose.connection.host);
     console.log("Con 1 "+ mongoose.connection.port);
+    console.log("OpenshiftDetails: "+ process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+    process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
+    process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
+    process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
+    process.env.OPENSHIFT_APP_NAME);
 
     return db;
 };
