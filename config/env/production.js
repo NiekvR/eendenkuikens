917eb84adb76457063ba5735ac76e5a83198f4a3
NiekvR <Niek@Nieks-MacBook-Pro.local>
2016-05-03 21:10:38 +0200

    Change in mongodb link

--- config/env/production.js
+++ config/env/production.js
@@ -1,7 +1,7 @@
 var connectionturi = 'localhost/test';
 
 if(process.env.OPENSHIFT_MONGODB_DB_PASSWORD){
-    connection_string = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
+    connectionturi = process.env.OPENSHIFT_MONGODB_DB_USERNAME + ":" +
         process.env.OPENSHIFT_MONGODB_DB_PASSWORD + "@" +
         process.env.OPENSHIFT_MONGODB_DB_HOST + ':' +
         process.env.OPENSHIFT_MONGODB_DB_PORT + '/' +
