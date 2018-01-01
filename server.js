3d9451c404c1f4014f9cca67003d5188a1dc2051
Niek van Rabenswaaij <Niek@Nieks-MacBook-Pro-2.local>
2018-01-01 11:10:40 +0100

    Add logging

--- server.js
+++ server.js
@@ -6,12 +6,14 @@ var mongoose = require('./config/mongoose'),
     express = require('./config/express'),
     passports = require('./config/passport');
 
+console.log('process.env', process.env);
+
 var db = mongoose();
 var app = express();
 var passport = passports();
 var ip = process.env.IP || 'localhost';
 var port = process.env.PORT || 3000;
-app.listen(port, ip);
+app.listen(port);
 module.exports = app;
 
 console.log('Server running at http://'+ip+':'+port);
