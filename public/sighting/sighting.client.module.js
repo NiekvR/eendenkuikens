4db0d8fe691cc81381742f212ab87660087d09a8
Niek van Rabenswaaij <>
2019-12-27 17:25:33 +0100

    Remove api-key from repo

--- public/sighting/sighting.client.module.js
+++ public/sighting/sighting.client.module.js
@@ -1,8 +1,3 @@
 (function() {
     var app = angular.module('sighting', []);
-
-    app.constant('GLOBALS', {
-        APIKEY:'AIzaSyC5eDItm0yolchF4-WqFd_9uZ_PNcjw0VQ',
-        url:'https://maps.googleapis.com/maps/api/js?key=API_KEY&callback=initMap'
-    });
 })();
